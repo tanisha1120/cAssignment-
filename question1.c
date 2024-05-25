e2fd960c8c9df6a082e61816c37bd9a72aadb629
tanisha1120 <tanishasingh.cse23@kgikanpur.in>
2024-05-25 23:22:58 +0530

    Update and rename question1.c to question10.c

--- question1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // Print "Hello KGI !!" on the console window.
-    //tanishaSingh
-    printf("Hello KGI !!\n");
-    return 0;
-}
---
+++ question10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    //tanisha singh
+    
+    float radius , volume;
+
+    printf("Enter the radius of sphere here");
+    scanf("%f",&radius);
+
+    volume = (4*3.1415926*(radius*radius*radius))/3;
+
+    printf("The volume of the sphere is %f ",volume);
+
+}
+
