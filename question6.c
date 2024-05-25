f297976d4320513327d4aa5243aba16c2790a099
tanisha1120 <tanishasingh.cse23@kgikanpur.in>
2024-05-25 13:39:56 +0530

    Update and rename question6 to question6.c

---
+++ question6.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+    // WAP to print sum of two numbers. Numbers are taken by user.
+    //tanishaSingh
+    int num1, num2;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    printf("Sum: %d\n", num1 + num2);
+    return 0;
+}
